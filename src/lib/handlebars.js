63e4d41c37ca848597d826368fae9642ad0715c5
Jefferson56 <jemasapi56@gmail.com>
2023-06-11 22:34:19 -0500

    Cambio en el formateo de la fecha en la vista de detalles del proyecto

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -10,5 +10,17 @@ helpers.eq= (a, b) => {
     return a === b;
 }
 
+helpers.formatDate= (date) => {
+        const inDate= new Date(date);
+        const year = inDate.getFullYear();
+        const month = String(inDate.getMonth() + 1).padStart(2, '0');
+        const day = String(inDate.getDate()).padStart(2, '0');
+  
+        // Formato deseado: "yyyy-mm-dd HH:MM:SS"
+        const formattedDate = `${year}-${month}-${day}`;
+  
+        return formattedDate;
+};
+
 module.exports= helpers;
 //Método utilizado para formatear el tiempo
