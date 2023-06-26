f96de3f8f04ce6088e44cc2e3958a5bf56a576ed
Dtr1gos18 <diego.trigos1805@gmail.com>
2023-06-25 21:33:28 -0500

    Se cambian las variables de entorno para hacer la conexion con la base de datos desplegada en alwaysdata

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports= { //Se exportan los datos para establecer la conexión con la base de datos
     database:{
-        host: 'localhost',
-        user: 'jefferson',
-        password: 'azbxcy123',
-        database: 'database_projectbank'
+        host: 'mysql-dtrigos18.alwaysdata.net',
+        user: 'dtrigos18',
+        password: 'Colombia2023',
+        database: 'dtrigos18_projectbank'
     }
 }
